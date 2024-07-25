742f99e904445220ef06d0d6524409d4f0c6deb1
TarasovOM <tarasovom@mail.ru>
2024-07-25 10:35:52 +0300

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
+    }
+}
